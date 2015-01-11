afd57b4a1ab7fbcd66f6223d60578aa55dcd943b
Per Andre Rønsen <perandre@gmail.com>
2015-01-11 20:05:39 +0100

    BS theme

---
+++ js/custom.js
@@ -0,0 +1,157 @@
+/*Theme    : assan
+ * Author  : Design_mylife
+ * Version : V1.0
+ * 
+ */
+
+
+
+//sticky header on scroll
+$(window).load(function() {
+    $(".sticky").sticky({topSpacing: 0});
+});
+
+
+
+//parallax
+$(window).stellar({
+    horizontalScrolling: false,
+    responsive: true/*,
+     scrollProperty: 'scroll',
+     parallaxElements: false,
+     horizontalScrolling: false,
+     horizontalOffset: 0,
+     verticalOffset: 0*/
+});
+
+//owl carousel for work
+$(document).ready(function() {
+
+    $("#work-carousel").owlCarousel({
+        // Most important owl features
+        items: 3,
+        itemsCustom: false,
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [980, 3],
+        itemsTablet: [768, 3],
+        itemsTabletSmall: false,
+        itemsMobile: [479, 1],
+        singleItem: false,
+        startDragging: true,
+        autoPlay: 6000
+    });
+
+});
+
+
+//owl carousel for news
+$(document).ready(function() {
+
+    $("#news-carousel").owlCarousel({
+        // Most important owl features
+        items: 2,
+        itemsCustom: false,
+        itemsDesktop: [1199, 2],
+        itemsDesktopSmall: [980, 2],
+        itemsTablet: [768, 2],
+        itemsTabletSmall: false,
+        itemsMobile: [479, 1],
+        singleItem: false,
+        startDragging: true,
+        autoPlay: 4000
+    });
+
+});
+
+
+
+//owl carousel for testimonials
+$(document).ready(function() {
+
+    $("#testi-carousel").owlCarousel({
+        // Most important owl features
+        items: 1,
+        itemsCustom: false,
+        itemsDesktop: [1199, 1],
+        itemsDesktopSmall: [980, 1],
+        itemsTablet: [768, 1],
+        itemsTabletSmall: false,
+        itemsMobile: [479, 1],
+        singleItem: false,
+        startDragging: true,
+        autoPlay: 4000
+    });
+
+});
+
+/* ==============================================
+ Counter Up
+ =============================================== */
+jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 100,
+        time: 800
+    });
+});
+
+
+/* ==============================================
+ WOW plugin triggers animate.css on scroll
+ =============================================== */
+
+var wow = new WOW(
+        {
+            boxClass: 'wow', // animated element css class (default is wow)
+            animateClass: 'animated', // animation css class (default is animated)
+            offset: 100, // distance to the element when triggering the animation (default is 0)
+            mobile: false        // trigger animations on mobile devices (true is default)
+        }
+);
+wow.init();
+
+
+//portfolio mix
+$('#grid').mixitup();
+
+
+
+
+//MAGNIFIC POPUP
+$('.show-image').magnificPopup({type: 'image'});
+
+/* ==============================================
+ flex slider
+ =============================================== */
+
+$('.main-flex-slider').flexslider({
+    slideshowSpeed: 5000,
+    directionNav: false,
+    animation: "fade"
+});
+
+//OWL CAROUSEL
+$("#clients-slider").owlCarousel({
+    autoPlay: 3000,
+    pagination: false,
+    items: 4,
+    itemsDesktop: [1199, 3],
+    itemsDesktopSmall: [991, 2]
+});
+
+
+/*========tooltip and popovers====*/
+
+$("[data-toggle=popover]").popover();
+
+$("[data-toggle=tooltip]").tooltip();
+
+/* ==============================================
+mb.YTPlayer
+=============================================== */
+
+jQuery(function(){
+			jQuery(".player").mb_YTPlayer();
+		});
+
+
+
